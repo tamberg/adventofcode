c61926854047b5d90a2bd83e63925afd10d4cc0d
tamberg <tamberg@yaler.net>
2022-12-11 15:01:14 +0100

    Update.

--- 2022/03/solve.c
+++ 2022/03/solve.c
@@ -15,19 +15,6 @@
 int items_min_pos[2 * 26]; // a - z, A - Z
 int items_max_pos[2 * 26];
 
-char read_char() {
-    char buf[1];
-    int r = read(STDIN_FILENO, buf, 1);
-    if (r == 1) {
-        return buf[0];
-    } else if (r == 0) {
-        return 0;
-    } else {
-        perror("read");
-        exit(-1);
-    }
-}
-
 int min(int a, int b) {
     return a < b ? a : b;
 }
@@ -49,6 +36,19 @@ int char_to_index(char ch) {
 
 char index_to_char(int i) {
     return i < 26 ? 'a' + i : 'A' + i - 26;
+}
+
+char read_char() {
+    char buf[1];
+    int r = read(STDIN_FILENO, buf, 1);
+    if (r == 1) {
+        return buf[0];
+    } else if (r == 0) {
+        return 0;
+    } else {
+        perror("read");
+        exit(-1);
+    }
 }
 
 int main() {
