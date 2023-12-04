efdd97e0c0d6ae1ccdd2a7647961082607400a7d
tamberg <tamberg@yaler.net>
2023-12-04 20:08:04 +0100

    Update.

--- 2023/03/solve.c
+++ 2023/03/solve.c
@@ -10,9 +10,10 @@
 // $ ./solve < input.txt
 // ?
 
-// 467..114..
-// ...*......
-// ..35..633.
+// 0-------->
+// 467..114.. 0
+// ...*...... |
+// ..35..633. v
 // ......#...
 // 617*......
 // .....+.58.
@@ -20,6 +21,87 @@
 // ......755.
 // ...$.*....
 // .664.598..
+
+// sliding window, hx = ..\n, hy = 3
+// bounding box, collision check
+
+/*
+
+struct num {
+    int row;
+    int col;
+    int val;
+    int len;
+}
+
+int engine[n_rows][n_cols];
+
+int is_part(struct num *x) {
+    return
+        is_symbol(engine[x->row - 1][x->col - 1 .. x->col + x->len]) ||
+        is_symbol(engine[x->row][x->col - 1]) ||
+        is_symbol(engine[x->row][x->col + x->len]) ||
+        is_symbol(engine[x->row + 1][x->col - 1 .. x->col + x->len]);
+}
+
+*/
+
+struct number {
+    int pos;
+    int len;
+    int val;
+    int is_part;
+}
+
+struct symbol {
+    int pos;
+    char ch;
+}
+
+struct line {
+    struct number *numbers;
+    struct symbol *symbols;
+}
+
+struct input {
+    struct line *prev;
+    struct line *curr;
+    struct line *next;
+}
+
+void add_line(struct input *i, struct line *l) {
+    free_line(i->prev);
+    i->prev = i->curr;
+    i->curr = i->l;
+}
+
+int is_adjacent(struct number *n, struct symbol *s) {
+    // sssss
+    //  nnn
+    //  ^
+    int result =
+        s->pos >= (n->pos - 1) &&
+        s->pos <= (n->pos + n->len);
+}
+
+int check_curr(struct input *i) {
+    assert(i != NULL);
+    assert(i->curr != NULL);
+    for (n in i->curr->numbers) {
+        if (i->prev != NULL) {
+            struct symbol *s = i->prev->symbols;
+            while (s != NULL) {
+                n->is_part = n->is_part || is_adjacent(n, s);
+                s = s->next;
+            }
+        }
+        struct symbol *s = i->curr->symbols;
+        while (s != NULL) {
+            n->is_part = n->is_part || is_adjacent(n, s);
+            s = s->next;
+        }
+    }
+}
 
 char read_char() {
     char buf[1];
