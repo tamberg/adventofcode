bc7f64dfd3109ff15313179615e3d358018d9815
tamberg <tamberg@yaler.net>
2023-12-07 23:46:33 +0100

    Update.

--- 2023/04/solve2.c
+++ 2023/04/solve2.c
@@ -6,10 +6,10 @@
 // https://adventofcode.com/2023/day/4
 
 // $ ./solve2 < test.txt
-// ? (30)
+// 30
 
 // $ ./solve2 < input.txt
-// ?
+// 5921508
 
 struct number {
     int val;
@@ -18,6 +18,7 @@ struct number {
 };
 
 struct card {
+    int id;
     int nof_instances;
     struct number *wins;
     struct number *mine;
@@ -58,10 +59,35 @@ char read_value(int *value) {
     return ch;
 }
 
-struct card *card_at(struct card *list, int i) {
-    struct card *c = list;
-    while (c != NULL && i != 0) {
-        i--;
+char skip_string(char *s) {
+    assert(s != NULL);
+    char ch = read_char();
+    while (*s != '\0') {
+        assert(ch == *s);
+        ch = read_char();
+        s++;
+    }
+    return ch;
+}
+
+void append_card(struct input *i, struct card *x) {
+    assert(x->next == NULL);
+    if (i->cards == NULL) {
+        i->cards = x;
+    } else {
+        struct card *c = i->cards;
+        struct card *d = NULL;
+        while (c != NULL) {
+            d = c;
+            c = c->next;
+        }
+        d->next = x;
+    }
+}
+
+struct card *get_card(struct input *i, int id) {
+    struct card *c = i->cards;
+    while (c != NULL && c->id != id) {
         c = c->next;
     }
     return c;
@@ -71,25 +97,23 @@ int eval2(struct input *i) {
     int result;
     struct card *c = i->cards;
     while(c != NULL) {
+        //printf("card %d\n", c->id);
+        int offset = 1;
         int score = 0;
         struct number *m = c->mine;
         while (m != NULL) {
             struct number *n = c->wins;
             while (n != NULL) {
                 if (m->val == n->val) {
-                    //printf("%d ", m->val);
-                    if (score == 0) {
-                        score = 1;
-                    } else {
-                        score *= 2;
-                    }
+                    struct card *x = get_card(i, c->id + offset);
+                    x->nof_instances += c->nof_instances;
+                    offset++;
                 }
                 n = n->next;
             }
             m = m->next;
         }
-        //printf("\n");
-        result += score;
+        result += c->nof_instances;
         c = c->next;
     }
     return result;
@@ -100,11 +124,14 @@ int main() {
     char ch = read_char();
     while (ch != '\n' && ch != 0) {
         struct card *c = malloc(sizeof(struct card));
+        c->nof_instances = 1;
         c->wins = NULL;
         c->mine = NULL;
-        while (ch != ':') {
-            ch = read_char();
-        }
+        c->next = NULL;
+        assert(ch == 'C');
+        ch = skip_string("ard");
+        ch = read_value(&c->id);
+        assert(ch == ':');
         ch = read_char();
         assert(ch == ' ');
         while (ch != '|') {
@@ -121,8 +148,7 @@ int main() {
             c->mine = n;
             assert(ch == ' ' || ch == '\n' || ch == 0);
         }
-        c->next = i->cards;
-        i->cards = c;
+        append_card(i, c);
         ch = read_char();
     }
     int result = eval2(i);
